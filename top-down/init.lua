db9f4c21ba15bd01096af91fa5d26ff3d441e548
Fabricio <fabricio@fabricio.org>
2024-05-17 17:01:29 -0300

    fix lsp warnings on init.lua when using top-down

--- top-down/init.lua
+++ top-down/init.lua
@@ -1,5 +1,6 @@
 --[[
 
+
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -588,7 +589,11 @@ require('lazy').setup({
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+                --disable = { 'missing-fields' }
+              },
             },
           },
         },
